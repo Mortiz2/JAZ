542650f4e99503ae1e01053c96f0459bcbf386a1
krzysztofprobulski <krzysztof.probulski@gmail.com>
2025-03-28 12:40:07 +0100

    Added REST api and custom banner

--- krzpro/src/main/java/pl/pjatk/krzpro/KrzproApplication.java
+++ krzpro/src/main/java/pl/pjatk/krzpro/KrzproApplication.java
@@ -2,8 +2,10 @@ package pl.pjatk.krzpro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = MessageController.class)
 public class KrzproApplication {
 
     public static void main(String[] args) {
